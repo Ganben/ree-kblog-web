90e9db129510e4da28762dfab01eb4b57d59ab79
ganben <caozhao@gmail.com>
2023-08-09 13:12:35 +0800

    first commit, tutorial done

---
+++ pages/api/post/[id].ts
@@ -0,0 +1,23 @@
+import prisma from '../../../lib/prisma';
+
+async function deletePost(id: string): Promise<void> {
+    await fetch(`/api/post/${id}`, {
+      method: 'DELETE',
+    });
+    Router.push('/');
+  }
+
+// DELETE /api/post/:id
+export default async function handle(req, res) {
+  const postId = req.query.id;
+  if (req.method === 'DELETE') {
+    const post = await prisma.post.delete({
+      where: { id: postId },
+    });
+    res.json(post);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`,
+    );
+  }
+}
